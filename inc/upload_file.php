1bf6d60457cd0f4f5c422b060c4687c7e42df19c
hirad <m.j.ahmadi88@gmail.com>
2017-10-13 16:33:08 +0330

    n

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -3,13 +3,13 @@ $company=$_POST['copmany'];
 $cat=$_POST['categoury'];
 $sn=$_POST['sn'];
 $pic=$_FILES['pic'];
-echo plugins_url();
+$url="http://hirad-co.com/wp-content/plugins/hirad_shoping/upload";
 if ($_FILES[$pic]["error"] > 0)
 	echo $_FILES[$pic]["error"];
     
   else               
   {                
-    if (file_exists("/upload". $_FILES[$pic]["name"]))  
+    if (file_exists($url. $_FILES[$pic]["name"]))  
 		echo $_FILES[$pic]["name"]. "already exists";
     
                    
@@ -18,7 +18,7 @@ if ($_FILES[$pic]["error"] > 0)
 		echo $_FILES[$pic]["name"]."has been uploaded.";
     
       move_uploaded_file($_FILES[$pic]["tmp_name"],
-      "/upload" . $_FILES[$pic]["name"]);
+      $url . $_FILES[$pic]["name"]);
       echo $_FILES[$pic]["name"]. "has been stor.";
     }                
   }
