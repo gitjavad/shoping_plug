08245027209864ee6281a8f79f0d350cb722d12c
hirad <m.j.ahmadi88@gmail.com>
2017-10-13 16:51:26 +0330

    n

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -3,7 +3,7 @@ $company=$_POST['copmany'];
 $cat=$_POST['categoury'];
 $sn=$_POST['sn'];
 ;
-$url="http://hirad-co.com/wp-content/plugins/hirad_shoping/upload";
+$url="http://hirad-co.com/wp-content/plugins/hirad_shoping/upload/";
 if ($_FILES["pic"]["error"] > 0)
 	echo $_FILES["pic"]["error"];
     
@@ -15,10 +15,8 @@ if ($_FILES["pic"]["error"] > 0)
                    
     else               
     {   
-		echo $_FILES["pic"]["name"]."has been uploaded.";
     
-      move_uploaded_file($_FILES["pic"]["tmp_name"],
-      $url . $_FILES["pic"]["name"]);
+      move_uploaded_file($_FILES["pic"]["tmp_name"], $url . $_FILES["pic"]["name"]);
       echo $_FILES["pic"]["name"]. "has been stor.";
     }                
   }
