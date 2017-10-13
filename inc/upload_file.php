341af8ed00f8580b6b4c6229d437b14c379513ca
hirad <m.j.ahmadi88@gmail.com>
2017-10-13 16:14:56 +0330

    n

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -3,14 +3,14 @@ $company=$_POST['copmany'];
 $cat=$_POST['categoury'];
 $sn=$_POST['sn'];
 $pic=$_FILES['pic'];
- $plugin_url=plugins_url( 'upload/', __FILE__ );
+ $plugin_url=plugins_url()."/upload";
 echo($pic);
 if ($_FILES[$pic]["error"] > 0)
 	echo "<script>console.log( 'Debug Objects: " .$_FILES[$pic]["error"]. "' );</script>";
     
   else               
   {                
-    if (file_exists($plugin_url . $_FILES[$pic]["name"]))  
+    if (file_exists($plugin_url. $_FILES[$pic]["name"]))  
 		echo "<script>console.log( 'Debug Objects: " .$_FILES[$pic]["name"]. "already exists );</script>";
     
                    
