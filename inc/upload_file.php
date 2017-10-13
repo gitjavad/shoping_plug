b3da9696a9447461d1f7c20ea1979c301eb849be
hirad <m.j.ahmadi88@gmail.com>
2017-10-13 17:14:02 +0330

    n

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -4,13 +4,46 @@ $cat=$_POST['categoury'];
 $sn=$_POST['sn'];
 
 $url="http://hirad-co.com/wp-content/plugins/hirad_shoping/upload/";
-
-    $destination_path = getcwd().DIRECTORY_SEPARATOR;
-$target_path = $destination_path . basename( $_FILES["pic"]["name"]);
-echo($taeget_path);
-      move_uploaded_file($_FILES["pic"]["tmp_name"], $target_path);
-      echo $_FILES["pic"]["name"]. "has been stor.";
-                    
-  
-    
-
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["pic"]["name"]);
+printf($target_file);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["pic"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["pic"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["pic"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["pic"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
