fdc97a481d5f0e7eff3725e802540f9062540898
hirad <m.j.ahmadi88@gmail.com>
2017-10-13 16:28:00 +0330

    n

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -3,24 +3,22 @@ $company=$_POST['copmany'];
 $cat=$_POST['categoury'];
 $sn=$_POST['sn'];
 $pic=$_FILES['pic'];
-
-print_r($pic);
 if ($_FILES[$pic]["error"] > 0)
-	echo "<script>console.log( 'Debug Objects: " .$_FILES[$pic]["error"]. "' );</script>";
+	echo $_FILES[$pic]["error"];
     
   else               
   {                
     if (file_exists("/upload". $_FILES[$pic]["name"]))  
-		echo "<script>console.log( 'Debug Objects: " .$_FILES[$pic]["name"]. "already exists );</script>";
+		echo $_FILES[$pic]["name"]. "already exists";
     
                    
     else               
     {   
-		echo "<script>console.log(" .$_FILES[$pic]["name"]. "has been uploaded.);</script>";
+		echo $_FILES[$pic]["name"]."has been uploaded.";
     
       move_uploaded_file($_FILES[$pic]["tmp_name"],
       "/upload" . $_FILES[$pic]["name"]);
-      echo "<script>console.log(" .$_FILES[$pic]["name"]. "has been stor.);</script>";
+      echo $_FILES[$pic]["name"]. "has been stor.";
     }                
   }
     
