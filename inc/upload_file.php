ebd4c92cd5fc03a8456cc75f365f7be3d77c5df9
hirad <m.j.ahmadi88@gmail.com>
2017-10-13 17:01:29 +0330

    n

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -7,6 +7,7 @@ $url="http://hirad-co.com/wp-content/plugins/hirad_shoping/upload/";
 
     $destination_path = getcwd().DIRECTORY_SEPARATOR;
 $target_path = $destination_path . basename( $_FILES["pic"]["name"]);
+echo($taeget_path);
       move_uploaded_file($_FILES["pic"]["tmp_name"], $target_path);
       echo $_FILES["pic"]["name"]. "has been stor.";
                     
