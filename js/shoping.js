78c48e49700e0314f20339a035b8aee8b9b73b15
hirad <m.j.ahmadi88@gmail.com>
2017-10-14 18:15:36 +0330

    nn

---
+++ js/shoping.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('form').submit(function (event) {
+
+
+        var formData = {
+            'pic': $('input[name=pic]').val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: 'inc/upload_file.php',
+            data: formData,
+            dataType: 'json',
+            encode: true
+        }).done(function (data) {
+            console.log(data);
+
+            if (!data.success) {
+
+           console.log("oh no")
+            } else {
+                console.log("oh yes")
+
+            }
+        });
+    });
+});
