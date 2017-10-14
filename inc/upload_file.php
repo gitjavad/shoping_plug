295c6672532c8b96c89dc35591684c2f6397c0e6
hirad <m.j.ahmadi88@gmail.com>
2017-10-14 18:00:57 +0330

    nn

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -1,5 +1,9 @@
 <?php
 
-
-
-move_uploaded_file($_FILES['pic']['tmp_name'],'/up');
+$target_dir = "up/";
+$target_file = $target_dir . basename($_FILES["pic"]["name"]);
+if (move_uploaded_file($_FILES["pic"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["pic"]["name"]). " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
