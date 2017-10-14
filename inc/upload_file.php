0c8447c9cde54b9b52ea16e63c6c6f0245bca201
hirad <m.j.ahmadi88@gmail.com>
2017-10-14 22:10:33 +0330

    nn

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -3,7 +3,10 @@
 $target_dir = "../img/";
 $target_file = $target_dir . basename($_FILES["pic"]["name"]);
 if (move_uploaded_file($_FILES["pic"]["tmp_name"], $target_file)) {
-    echo "The file ". basename( $_FILES["pic"]["name"]). " has been uploaded.";
+    /*echo "The file ". basename( $_FILES["pic"]["name"]). " has been uploaded.";*/
+    echo $json = array('success'=>'true');
 } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo $json = array('error'=>'Sorry, there was an error uploading your file.');
+
 }
+echo json_encode($json);
