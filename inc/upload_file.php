e1110f2ac15d77c100424396bc640b3ac291bb2a
hirad <m.j.ahmadi88@gmail.com>
2017-10-14 17:40:02 +0330

    nn

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -2,4 +2,4 @@
 
 
 
-move_uploaded_file($_FILES['pic']['tmp_name'], sprintf('./uploads/%s.%s', sha1_file($_FILES['upfile']['tmp_name'])));
+move_uploaded_file($_FILES['pic']['tmp_name'], sprintf('./uploads/%s.%s', sha1_file($_FILES['pic']['tmp_name'])));
