0e4f7ae4f8556ef26033d18936faef312bd31603
hirad <m.j.ahmadi88@gmail.com>
2017-10-21 01:26:11 +0330

    n

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -1,20 +1,8 @@
 <?php
 $cat = $_POST['categoury'];
-$target_dir = "../img/";
-$target_file = $target_dir . basename($_FILES["pic"]["name"]);
-$pic='http://hirad-co.com/wp-content/plugins/hirad_shoping/img/'.basename($_FILES["pic"]["name"]);
-if (move_uploaded_file($_FILES["pic"]["tmp_name"], $target_file)) {
-
-    echo '{ "message": "' . basename( $_FILES["pic"]["name"]) . '" }';
-
-} else {
-    echo '{ "message": "no" }';
-
-}
-/*$total = count($_FILES['pic']['name']);
+$total = count($_FILES['pic']['name']);
 $target_dir = "../img/";
 // Loop through each file
-echo '{ "message": "' . $total . '" }';
 for($i=0; $i<$total; $i++) {
     //Get the temp file path
     $tmpFilePath = $_FILES['pic']['tmp_name'][$i];
@@ -26,35 +14,34 @@ for($i=0; $i<$total; $i++) {
 
         //Upload the file into the temp dir
         if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-           echo '{ "message": "' . $_POST['categoury'] . '" }';
 
-                        $servername = "localhost";
-                        $username = "hirad_admin15023";
-                        $password = "9133647736!@#";
-                        $dbname = "hirad-co_com_site";
-                        $company=dirname($_FILES["pic"]);
-                        $sn=basename($_FILES["pic"]["name"]);
-                        $pic=$newFilePath;
-                        $conn = new mysqli($servername, $username, $password, $dbname);
+            $servername = "localhost";
+            $username = "hirad_admin15023";
+            $password = "9133647736!@#";
+            $dbname = "hirad-co_com_site";
+            $company=dirname($_FILES["pic"]);
+            $sn=basename($_FILES["pic"]["name"]);
+            $pic=$newFilePath;
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
-                        if ($conn->connect_error) {
-                            die("Connection failed: " . $conn->connect_error);
-                        }
+            if ($conn->connect_error) {
+                echo '{ "message": "'.die($conn->connect_error).' "}';
+            }
 
-                        $sql = "INSERT INTO wp_hirad_shoping (company, cat, sn, pic)
-            VALUES ($company, $cat, $sn, $pic)";
+            $sql = "INSERT INTO wp_hirad_shoping (company, cat, sn, pic)
+VALUES ($company, $cat, $sn, $pic)";
 
-                        if ($conn->query($sql) === TRUE) {
-                            echo "New record created successfully";
-                        } else {
-                            echo "Error: " . $sql . "<br>" . $conn->error;
-                        }
+            if ($conn->query($sql) === TRUE) {
+                echo '{ "message": "New record created successfully" }';
+            } else {
 
-                        $conn->close();
+                echo '{ "message": "' . $sql . '"<br>"' . $conn->error .'"}';
+            }
 
-                    }else{
-                        echo ('{message:ohno}');
-                    }
+            $conn->close();
+
+        }else{
+            echo '{ "message": "no" }';
         }
     }
 }
