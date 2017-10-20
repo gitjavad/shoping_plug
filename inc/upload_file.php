aa12d077372af66fb2c0d4bbf5c298a4b4e9c805
hirad <m.j.ahmadi88@gmail.com>
2017-10-21 02:53:25 +0330

    n

--- inc/upload_file.php
+++ inc/upload_file.php
@@ -30,7 +30,7 @@ for($i=0; $i<$total; $i++) {
             }
 
 
-            $sql = "INSERT INTO wp_hirad_shoping (company, cat, sn) VALUES ($company, $cat, $sn)";
+            $sql = "INSERT INTO wp_hirad_shoping (company, cat, sn) VALUES('".$company."', '".$cat."', '".$sn."')";;
 
             if ($conn->query($sql) === TRUE) {
                 echo '{ "message": "New record created successfully" }';
