8af749705988be989ad1509401a7b4f7f64f05e0
hirad <m.j.ahmadi88@gmail.com>
2017-10-21 01:13:21 +0330

    n

--- js/shoping.js
+++ js/shoping.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($){
                 $("#prog").attr('value',percentCompelete);
             },
             success:function (data) {
-                console.log(data.message)
+                console.log(data.message[0])
                 console.log("ha")
 
             }
